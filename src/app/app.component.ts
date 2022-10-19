212e33116f634c7f5994b45161e58d01aa69127a
Amos <amosakinwale4@gmail.com>
2022-10-19 10:21:35 +0100

    done with the budget app might add some extra features later

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'budgetapp';
+  public expense:string = ''
+  public price:any = 0
+  public duedate:any = ''
+  public budgetarray:any=[]
+  // public budgetindex:any = {}
+  public totalprice:any = 0
+  public budgetdetails:any = {expense:this.expense, price:this.price, duedate:this.duedate, totalprice:0}
+  
+  
+  addbudget(){
+    let budgetdetails = {expense:this.expense, price:this.price, duedate:this.duedate, totalprice:0}
+    console.log(budgetdetails)
+    this.totalprice= this.price + this.totalprice
+    console.log(this.totalprice)
+    this.budgetarray.push(budgetdetails)
+    console.log(this.budgetarray)
+    
+
+  }
+  delete(i:any){
+    this.budgetarray=this.budgetarray.filter((item:any, index:any)=>index!=i)
+    this.totalprice = this.totalprice - this.price
+  }
+
 }
+
